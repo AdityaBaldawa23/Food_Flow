3d418f5cff74d3414408f2c241dbbe021d6e7b65
AdityaBaldawa23 <adityabaldawa23@gmail.com>
2025-03-13 17:26:55 +0530

    Saving local changes before pulling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Home from './Pages/LandingPage/HomePage'
+import About from './Pages/AboutUs/AboutUs'
 import { Routes, Route } from "react-router-dom"
 import { BrowserRouter } from "react-router-dom"
 
@@ -8,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
       <Route path="/home" element={<Home />} />
-      <Route path="/about us" element={<Home />} />
+      <Route path="/aboutus" element={<About />} />
     </Routes>
     </BrowserRouter>
     </>
